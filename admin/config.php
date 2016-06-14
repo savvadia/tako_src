9ab38dd0dbc09982913a74e9164e3175f70abc42
Diana Savvatina <diana.savvatina@gmail.com>
2016-06-14 12:28:16 +0300

    config files

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tako-racing.com/admin/');
+define('HTTP_CATALOG', 'http://tako-racing.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://tako-racing.com/admin/');
+define('HTTPS_CATALOG', 'http://tako-racing.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/takoraci/public_html//admin/');
+define('DIR_SYSTEM', '/home/takoraci/public_html//system/');
+define('DIR_IMAGE', '/home/takoraci/public_html//image/');
+define('DIR_LANGUAGE', '/home/takoraci/public_html//admin/language/');
+define('DIR_TEMPLATE', '/home/takoraci/public_html//admin/view/template/');
+define('DIR_CONFIG', '/home/takoraci/public_html//system/config/');
+define('DIR_CACHE', '/home/takoraci/public_html//system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/takoraci/public_html//system/storage/download/');
+define('DIR_LOGS', '/home/takoraci/public_html//system/storage/logs/');
+define('DIR_MODIFICATION', '/home/takoraci/public_html//system/storage/modification/');
+define('DIR_UPLOAD', '/home/takoraci/public_html//system/storage/upload/');
+define('DIR_CATALOG', '/home/takoraci/public_html//catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'takoraci_o786');
+define('DB_PASSWORD', 't6yr5tRTY');
+define('DB_DATABASE', 'takoraci_o786');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://tako-racing.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://tako-racing.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/takoraci/public_html//catalog/');
+define('DIR_SYSTEM', '/home/takoraci/public_html//system/');
+define('DIR_IMAGE', '/home/takoraci/public_html//image/');
+define('DIR_LANGUAGE', '/home/takoraci/public_html//catalog/language/');
+define('DIR_TEMPLATE', '/home/takoraci/public_html//catalog/view/theme/');
+define('DIR_CONFIG', '/home/takoraci/public_html//system/config/');
+define('DIR_CACHE', '/home/takoraci/public_html//system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/takoraci/public_html//system/storage/download/');
+define('DIR_LOGS', '/home/takoraci/public_html//system/storage/logs/');
+define('DIR_MODIFICATION', '/home/takoraci/public_html//system/storage/modification/');
+define('DIR_UPLOAD', '/home/takoraci/public_html//system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'takoraci_o786');
+define('DB_PASSWORD', 't6yr5tRTY');
+define('DB_DATABASE', 'takoraci_o786');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
